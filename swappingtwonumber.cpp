28bbe82c1f5d5eb157ba4c07bf558b502c9772ef
randhir408 <randhirkumarsonukumar@gmail.com>
2020-08-18 22:29:27 +0530

    swapping

---
+++ swappingtwonumber.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{   
+   int a,b;
+   printf("Enter the value of a=");
+   scanf("%d",&a);
+   printf("Enter the value of b=");
+   scanf("%d",&b);
+   printf("before swapping a=%d and b=%d",a,b);
+   a=a+b;
+   b=a-b;
+   a=a-b;
+   printf("\nAfter  swapping a=%d and b=%d",a,b);
+   return 0;
+}
+    
+  
